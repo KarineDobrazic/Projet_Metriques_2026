c7b016055f647efe4188b51771ed3ffb45cb4c88
KarineDobrazic <karine.dobrazic@oclock.school>
2026-05-18 13:27:45 +0000

    ajout page et route histogramme

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ def api_paris():
 @app.route("/rapport")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme")
+def monhistogramme():
+    return render_template("histogramme.html")
 # Ne rien mettre après ce commentaire
     
 if __name__ == "__main__":
